f785d75f94801b839699b3b4e5492ace5937db25
Joyce <joycalation@gmail.com>
2020-03-29 19:04:43 -0400

    Changed port back

--- app.js
+++ app.js
@@ -2,10 +2,7 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const models = require('./models');
 
-const PORT = process.env.PORT;
-if (PORT == null || port == "") {
-  PORT = 8000;
-};
+const PORT = process.env.PORT || 8000;
 
 const app = express();
 
@@ -15,7 +12,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Load up all of the controllers
 const controllers = require('./controllers');
 app.use(controllers)
-
 
 // First, make sure the Database tables and models are in sync
 // then, start up the server and start listening.
