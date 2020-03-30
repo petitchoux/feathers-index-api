92fa10ee9fd972129b533b2b4bb5cf75cfff9403
Joyce <joycalation@gmail.com>
2020-03-29 23:04:43 -0400

    db edit

--- app.js
+++ app.js
@@ -32,17 +32,17 @@ client.query('SELECT table_schema,table_name FROM information_schema.tables;', (
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 // First, make sure the Database tables and models are in sync
 // then, start up the server and start listening.
 // Force should be false for security pursposes.
 // Change Force to true only when editing db
-models.sequelize.sync({force: false})
-  .then(() => {
-    app.listen(port, () => {
-      console.log(`Server is up and running on port: ${port}`)
-    });
-  });
+// models.sequelize.sync({force: false})
+//   .then(() => {
+//     app.listen(port, () => {
+//       console.log(`Server is up and running on port: ${port}`)
+//     });
+//   });
 
 const S3_BUCKET = process.env.S3_BUCKET;
