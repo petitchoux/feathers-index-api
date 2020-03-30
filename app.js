ca66e2ef843d07794c6f37a349b66702fa12c214
Joyce <joycalation@gmail.com>
2020-03-29 23:43:25 -0400

    Enabled Sequelize to work with Heroku

--- app.js
+++ app.js
@@ -28,19 +28,6 @@ app.use(controllers);
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
-// First, make sure the Database tables and models are in sync
-// then, start up the server and start listening.
-// Force should be false for security pursposes.
-// Change Force to true only when editing db
-// models.sequelize.sync({force: false})
-//   .then(() => {
-//     app.listen(port, () => {
-//       console.log(`Server is up and running on port: ${port}`)
-//     });
-//   });
-
 models.sequelize.sync().then(function() {
   http.createServer(app).listen(port, function(){
     console.log('Express server listening on port ' + port);
